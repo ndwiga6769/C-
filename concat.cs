2c48faa1e1acd704064cbe3e9247d5af93874153
ndwiga6769 <your.email@example.com>
2024-10-19 13:45:47 +0300

    codes

---
+++ concat.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+	class Program
+    {
+    static void Main(string[] args)
+    {
+    string FirstName = "Danwycliff ";
+    string lastName = "Njoka Ndwiga";
+    Console.WriteLine("My Name is " + FirstName + lastName);
+    }
+    }
+    
+}
