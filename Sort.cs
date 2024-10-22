dbdf4d67cddb92ba4a68535360ed1cc7374fa7c1
ndwiga6769 <your.email@example.com>
2024-10-22 11:34:14 +0300

    codes

---
+++ Sort.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] myNumbers = {5, 1, 8, 9};
+      Console.WriteLine(myNumbers.Max());
+      Console.WriteLine(myNumbers.Min());
+      Console.WriteLine(myNumbers.Sum());
+    }
+  }
+}
