c2498b8dc929634fff63c52f62d2777cad36c867
ndwiga6769 <your.email@example.com>
2024-10-22 09:37:21 +0300

    codes

---
+++ DoWhile.cs
@@ -0,0 +1,10 @@
+// This loop will execute a code block once, before checking if the
+// condition is true, then it will repeat the condition as long as the conditionis true
+
+int  i = 0
+do 
+{
+    Console.WriteLine(i)
+    i++
+}
+while (i<5)
