226bf2bdbd850e18834cdea51eb29642c7bf3d9d
ndwiga6769 <your.email@example.com>
2024-10-22 09:28:26 +0300

    codes

---
+++ whileLoop.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace myApplication
+	{
+    class Program
+    	{
+        static void Main(string[] args)
+        	{
+            int i = 0;
+            while(i < 10000)
+            	{
+                Console.WriteLine(i);
+                i++;
+                }
+            }
+        }
+    }
