e222af9d169cf1b6d3fb0f8feac36bfa3c3254f2
ndwiga6769 <your.email@example.com>
2024-10-22 16:54:09 +0300

    codes

--- Parameters.cs
+++ Parameters.cs
@@ -16,3 +16,22 @@ namespace myApplication
     }
     }
 }
+
+// we can also call a method with a default argument we use the = operator and we pass it in the argument section
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+  static void Name(string country = "kenya by default")
+    {
+    Console.WriteLine("My country is " + country);
+    }
+  static void Main(string[] args)
+  	{
+    Name("Uganda");
+    Name();
+    }
+  }
+}
