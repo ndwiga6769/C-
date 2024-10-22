0658936f6f558991bc37565949bff227b354aba2
ndwiga6769 <your.email@example.com>
2024-10-22 17:17:08 +0300

    codes

---
+++ Return.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static int Addition(int x, int y)
+    {
+    return x + y;
+    }
+    static void Main(string[] args)
+    {
+    Console.WriteLine(Addition(6,7));
+    }
+  }
+}
